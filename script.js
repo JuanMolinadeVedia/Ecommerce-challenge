32cb6aee206f969f5c15a497c7ff81b36e587087
GM2 Academy <gm2academy@Tecnicos-MacBook-Pro.local>
2023-08-31 09:58:37 -0300

    feat(sricpt.js): add new cards logic

--- script.js
+++ script.js
@@ -70,19 +70,66 @@ function showProductAddedMessage() {
         document.body.removeChild(feedbackDiv);
     }, 3000);
 }
-
+function createDOMElement(tag, classNames = []) {
+    const element = document.createElement(tag);
+    element.classList.add(...classNames);
+    return element;
+}
 function addProductCard(product) {
-    const productCard = document.createElement("div");
-    productCard.classList.add("product-card");
-    productCard.innerHTML = `
-        <img class="imgs" src="./imgs/${product.img}" alt="">
-        <h3 class="title">${product.title}</h3>
-        <p class="description">${product.description}</p>
-        <p class="price">Precio: $${product.price.toFixed(2)}</p>
-    `;
+    const productCard = createDOMElement("div", ["container"]);
+
+    const infoSection = createDOMElement("div", ["info"]);
+
+    const imgElement = createDOMElement("img");
+    imgElement.src = `./imgs/shoes/${product.img}`;
+    imgElement.alt = "";
+
+    const titleElement = createDOMElement("h3", ["title"]);
+    titleElement.textContent = product.title;
+
+    const idElement = createDOMElement("p");
+    idElement.textContent = "id number";
+
+    infoSection.appendChild(imgElement);
+    infoSection.appendChild(titleElement);
+    infoSection.appendChild(idElement);
+
+    const bottomSection = createDOMElement("div", ["bottom"]);
+
+    const addButton = createDOMElement("button", ["addCart"]);
+
+    addButton.classList.add("button-flex-container");
+
+    const shoppingBagIcon = createDOMElement("img");
+    shoppingBagIcon.src = "./imgs/card/shopping-bag.svg";
+    shoppingBagIcon.alt = "Shopping Bag";
+
+    const plusIcon = createDOMElement("img");
+    plusIcon.src = "./imgs/card/plus.svg";
+    plusIcon.alt = "Add to Cart";
+
+    addButton.appendChild(shoppingBagIcon);
+    addButton.appendChild(plusIcon);
+
+    const pricesDiv = createDOMElement("div", ["prices"]);
+
+    const listPrice = createDOMElement("p", ["listPrice"]);
+
+    const price = createDOMElement("p", ["price"]);
+    price.textContent = `$${product.price.toFixed(2)}`;
+
+    pricesDiv.appendChild(listPrice);
+    pricesDiv.appendChild(price);
+
+    bottomSection.appendChild(addButton);
+    bottomSection.appendChild(pricesDiv);
+
+    productCard.appendChild(infoSection);
+    productCard.appendChild(bottomSection);
 
     productCardContainer.appendChild(productCard);
 }
+
 
 function updateProductCards() {
     products.forEach(addProductCard);
@@ -97,3 +144,4 @@ darkModeToggle.addEventListener("change", function () {
         localStorage.removeItem("darkMode");
     }
 });
+addProductButton.addEventListener("click", addProductHandler);
