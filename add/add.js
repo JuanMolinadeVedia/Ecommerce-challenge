c73658fc8067cd6c5c119e143113c332a94878b7
GM2 Academy <gm2academy@Tecnicos-MacBook-Pro.local>
2023-09-01 11:27:48 -0300

    feat(add):when discount active icon on card

--- add/add.js
+++ add/add.js
@@ -1,46 +1,110 @@
-
-
+// Obtener elementos HTML por su ID o clase y asignarlos a variables
 const addProductForm = document.getElementById("productForm");
 const goBackLink = document.querySelector(".button-container a");
+const cardTypeToggle = document.getElementById("cardTypeToggle");
+const productCardContainer = document.querySelector(".product-card-container");
+const darkModeToggle = document.getElementById("darkModeToggle");
+const body = document.body;
+const darkModeToggleLabel = document.querySelector(".dark-mode-toggle label");
 
+// Crear una variable para almacenar los productos
+let products = [];
+
+// Manejar el evento de envío del formulario
 addProductForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-    addProductFromForm();
+    event.preventDefault(); // Prevenir el comportamiento predeterminado del formulario (recargar la página)
+    addProductFromForm(); // Llamar a la función para agregar un producto desde el formulario
 });
 
+// Manejar el enlace "Go Back"
 goBackLink.addEventListener("click", function (event) {
-    event.preventDefault();
-    window.location.href = "./../index.html"; 
+    event.preventDefault(); // Prevenir la acción predeterminada del enlace (navegar a otra página)
+    window.location.href = "./../index.html"; // Redirigir a la página principal
 });
 
+// Manejar el cambio en el toggle de tipo de tarjeta
+cardTypeToggle.addEventListener("change", function () {
+    if (cardTypeToggle.checked) {
+        // Si el checkbox está marcado, cambiar la clase CSS a "label-card"
+        productCardContainer.classList.add("label-card");
+    } else {
+        // Si el checkbox no está marcado, eliminar la clase CSS "label-card"
+        productCardContainer.classList.remove("label-card");
+    }
+});
 
+// Manejar el cambio en el toggle de modo oscuro
+darkModeToggle.addEventListener("change", function () {
+    if (darkModeToggle.checked) {
+        // Si el toggle está marcado, habilitar el modo oscuro
+        body.classList.add("dark-mode");
+        darkModeToggleLabel.textContent = "Light Mode"; // Cambiar el texto del label
+        darkModeToggleLabel.classList.add("dark-mode-label"); // Agregar una clase CSS
+        localStorage.setItem("darkMode", "enabled"); // Guardar el estado del modo oscuro en el almacenamiento local
+    } else {
+        // Si el toggle no está marcado, deshabilitar el modo oscuro
+        body.classList.remove("dark-mode");
+        darkModeToggleLabel.textContent = "Dark Mode"; // Restaurar el texto del label
+        darkModeToggleLabel.classList.remove("dark-mode-label"); // Eliminar la clase CSS
+        localStorage.removeItem("darkMode"); // Eliminar el estado del modo oscuro del almacenamiento local
+    }
+});
+
+// Función para agregar un producto desde el formulario
 function addProductFromForm() {
+    // Obtener valores del formulario
     const title = document.getElementById("title").value;
     const description = document.getElementById("description").value;
-    const price = parseFloat(document.getElementById("price").value);
+    let price = parseFloat(document.getElementById("price").value);
     const img = document.getElementById("img").value;
 
+    // Validar el precio ingresado
     if (isNaN(price) || price < 0) {
         alert("Please enter a valid positive numeric price.");
         return;
     }
 
-    // if (!img.match(/\.webp$/i)) {
-    //     alert("Please enter a valid image file name ('shoe-name.webp').");
-    //     return;
-    // }
+    // Verificar si el toggle de descuento está marcado
+    const discounted = cardTypeToggle.checked;
 
-    const product = createProduct(title, description, price, img);
+    // Aplicar un 10% de descuento al precio si es necesario
+    if (discounted) {
+        price = price - (price * 0.10);
+    }
+
+    // Crear un objeto de producto con los valores del formulario
+    const product = {
+        img,
+        title,
+        description,
+        price,
+        discounted
+    };
+
+    // Llamar a las funciones para agregar el producto a la lista, actualizar las tarjetas y guardar en el almacenamiento local
+    addProductCard(product);
     uploadProducts(product, products);
     updateProductCards();
     saveProductsToLocalStorage();
 }
 
+// Función para actualizar las tarjetas de productos en el contenedor
+function updateProductCards() {
+    productCardContainer.innerHTML = "";
+
+    for (let i = 0; i < products.length; i++) {
+        const product = products[i];
+        addProductCard(product);
+    }
+}
+
+// Función para guardar los productos en el almacenamiento local
 function saveProductsToLocalStorage() {
     const productsJSON = JSON.stringify(products);
     localStorage.setItem('products', productsJSON);
 }
 
+// Función para crear un objeto de producto
 function createProduct(title, description, price, img) {
     let product = {
         title,
@@ -51,63 +115,48 @@ function createProduct(title, description, price, img) {
     return product;
 }
 
+// Función para cargar productos en el array de productos
 function uploadProducts(product, array) {
     array.push(product);
     return array;
 }
 
-function updateProductCards() {
-    const productCardContainer = document.querySelector(".product-card-container");
-    productCardContainer.innerHTML = ""; 
-
-    for (let i = 0; i < products.length; i++) {
-        const product = products[i];
-        addProductCard(product.img, product.title, product.description, product.price);
-    }
-}
-
-function addProductCard(img, title, description, price) {
+// Función para agregar una tarjeta de producto al contenedor
+function addProductCard(product) {
     const productCard = document.createElement("div");
     productCard.classList.add("product-card");
+
+    const priceIcon = product.discounted ? '<i class="fas fa-percent"></i>' : '';
+
+    const priceString = product.price.toFixed(2) + (product.discounted ? "⭐" : '');
+
+    // Crear la estructura de la tarjeta de producto con los valores del objeto de producto
     productCard.innerHTML = `
-        <img class="imgs" src="./../imgs/shoes/${img}" alt="">
-        <h3 class="title">${title}</h3>
-        <p class="description">${description}</p>
-        <p class="price">Price: $${price.toFixed(2)}</p>
+        <img class="imgs" src="./../imgs/shoes/${product.img}" alt="">
+        <h3 class="title">${product.title}</h3>
+        <p class="description">${product.description}</p>
+        <p class="price">${priceIcon} Price: $${priceString}</p>
     `;
 
-    const productCardContainer = document.querySelector(".product-card-container");
+    // Agregar la tarjeta de producto al contenedor
     productCardContainer.appendChild(productCard);
 }
 
+// Evento de carga de la ventana
 window.addEventListener("load", function () {
-    products = JSON.parse(localStorage.getItem('products')) || [];
+    // Cargar productos desde el almacenamiento local o inicializar como un array vacío
+    const savedProductsJSON = localStorage.getItem('products');
+    if (savedProductsJSON) {
+        products = JSON.parse(savedProductsJSON);
+    }
     updateProductCards();
 
-    // Restore dark mode state
+    // Restaurar estado de modo oscuro
     const savedDarkMode = localStorage.getItem("darkMode");
     if (savedDarkMode === "enabled") {
         body.classList.add("dark-mode");
         darkModeToggle.checked = true;
         darkModeToggleLabel.textContent = "Light Mode";
         darkModeToggleLabel.classList.add("dark-mode-label");
-    }
-});
-
-const darkModeToggle = document.getElementById("darkModeToggle");
-const body = document.body;
-const darkModeToggleLabel = document.querySelector(".dark-mode-toggle label");
-
-darkModeToggle.addEventListener("change", function () {
-    if (darkModeToggle.checked) {
-        body.classList.add("dark-mode");
-        darkModeToggleLabel.textContent = "Light Mode";
-        darkModeToggleLabel.classList.add("dark-mode-label"); // Add dark mode class
-        localStorage.setItem("darkMode", "enabled"); // Save dark mode state
-    } else {
-        body.classList.remove("dark-mode");
-        darkModeToggleLabel.textContent = "Dark Mode";
-        darkModeToggleLabel.classList.remove("dark-mode-label"); // Remove dark mode class
-        localStorage.removeItem("darkMode"); // Remove dark mode state
     }
 });
