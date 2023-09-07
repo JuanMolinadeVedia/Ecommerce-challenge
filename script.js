e4529a82a02fe281b0363c6b336b1d9c3415c873
GM2 Academy <gm2academy@Tecnicos-MacBook-Pro.local>
2023-09-07 13:02:37 -0300

    feat(script): refactoring

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-// Selecting DOM elements
-// const addProductButton = document.getElementById("addProductButton");
+// Seleccionando elementos del DOM
 const darkModeToggle = document.getElementById("darkModeToggle");
 const body = document.body;
 const darkModeToggleLabel = document.querySelector(".dark-mode-toggle label");
 const productCardContainer = document.querySelector(".product-card-container");
 const productCardDiscountContainer = document.querySelector(".product-card-discount-container");
-
 let products = [];
 
 window.addEventListener("load", function () {
     loadProductsFromLocalStorage();
     loadDarkMode();
-    // addProductButton.addEventListener("click", addProductHandler);
-    fetchDataAndRenderProducts(); // Integrate the API call here
+    fetchDataAndRenderProducts();
 });
 
 async function loadProductsFromLocalStorage() {
@@ -32,9 +29,7 @@ async function getProductsFromAsyncFunction() {
 
 function loadDarkMode() {
     const savedDarkMode = localStorage.getItem("darkMode");
-    if (savedDarkMode === "enabled") {
-        enableDarkMode();
-    }
+    savedDarkMode === "enabled" ? enableDarkMode() : disableDarkMode();
 }
 
 function enableDarkMode() {
@@ -42,6 +37,9 @@ function enableDarkMode() {
     darkModeToggle.checked = true;
     darkModeToggleLabel.textContent = "Light Mode";
     darkModeToggleLabel.classList.add("dark-mode-label");
+
+    // Agregar evento change al checkbox
+    darkModeToggle.addEventListener("change", handleDarkModeToggleChange);
 }
 
 function disableDarkMode() {
@@ -49,6 +47,14 @@ function disableDarkMode() {
     darkModeToggle.checked = false;
     darkModeToggleLabel.textContent = "Dark Mode";
     darkModeToggleLabel.classList.remove("dark-mode-label");
+}
+
+function handleDarkModeToggleChange() {
+    if (darkModeToggle.checked) {
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
 }
 
 function createProduct(title, description, price, img, discounted = false) {
@@ -67,7 +73,7 @@ function saveProductsToLocalStorage(products) {
 
 function addProductHandler() {
     const newProduct = createProduct("Sample Title", "Sample Description", 9.99, "sample.jpg");
-    products.push(newProduct); // Use push to add the new product to the array
+    products.push(newProduct);
     saveProductsToLocalStorage(products);
     addProductCard(newProduct);
     showProductAddedMessage();
@@ -91,103 +97,133 @@ function createDOMElement(tag, classNames = []) {
 function addProductCard(product) {
     const productCard = document.createElement("div");
     productCard.classList.add("product-card");
-    const infoSection = document.createElement("div");
-    infoSection.classList.add("info");
-    const imgElement = document.createElement("img");
-    imgElement.classList.add("imgs");
-    const imgSrc = product.image;
-    imgElement.src = imgSrc;
-    imgElement.alt = "";
-    const titleElement = document.createElement("h3");
-    titleElement.classList.add("title");
-    titleElement.textContent = product.title;
-    const idElement = document.createElement("p");
-    idElement.textContent = "id number";
-    infoSection.appendChild(imgElement);
-    infoSection.appendChild(titleElement);
-    infoSection.appendChild(idElement);
-    const bottomSection = document.createElement("div");
-    bottomSection.classList.add("bottom");
-    const addButton = document.createElement("button");
-    addButton.classList.add("addCart", "button-flex-container");
-    const shoppingBagIcon = document.createElement("svg");
-    shoppingBagIcon.innerHTML = `
-        <svg id="shoppingBagIcon" xmlns="http://www.w3.org/2000/svg" width="24" height="26" viewBox="0 0 24 26" fill="none">
-    <g clip-path="url(#clip0_471_394)">
-        <path d="M5.36048 1.91846L2.11188 6.32375V21.7423C2.11188 22.3264 2.34005 22.8867 2.74621 23.2998C3.15236 23.7128 3.70322 23.9449 4.27761 23.9449H19.4378C20.0121 23.9449 20.5630 23.7128 20.9692 23.2998C21.3753 22.8867 21.6035 22.3264 21.6035 21.7423V6.32375L18.3549 1.91846H5.36048Z" stroke="white" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M2.11188 6.32379H21.6035" stroke="white" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M16.1892 10.729C16.1892 11.8974 15.7328 13.0179 14.9205 13.844C14.1082 14.6702 13.0065 15.1343 11.8577 15.1343C10.7089 15.1343 9.60718 14.6702 8.79487 13.844C7.98256 13.0179 7.52621 11.8974 7.52621 10.729" stroke="white" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
-    </g>
-    <defs>
-        <clipPath id="clip0_471_394">
-        <rect width="22.7402" height="25.6057" fill="white" transform="translate(0.487579 0.266479)"/>
-        </clipPath>
-    </defs>
-    </svg>
-    `;
-    shoppingBagIcon.alt = "Shopping Bag";
-    
-    const plusIcon = document.createElement("svg");
-    plusIcon.innerHTML = `
-    <svg id="plusIcon" xmlns="http://www.w3.org/2000/svg" width="13" height="12" viewBox="0 0 13 12" fill="none">
-    <g clip-path="url(#clip0_471_398)">
-      <path d="M6.3493 2.07715V9.7864" stroke="white" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
-      <path d="M2.55927 5.93176H10.1393" stroke="white" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
-    </g>
-    <defs>
-      <clipPath id="clip0_471_398">
-        <rect width="11.3701" height="10.7379" fill="white" transform="translate(0.664246 0.700439)"/>
-      </clipPath>
-    </defs>
-  </svg>
-    `;
-    plusIcon.alt = "Add to Cart";
-    addButton.appendChild(shoppingBagIcon);
-    addButton.appendChild(plusIcon);
-    const pricesDiv = document.createElement("div");
-    pricesDiv.classList.add("prices");
-    const listPrice = document.createElement("p");
-    listPrice.classList.add("listPrice");
-    const price = document.createElement("p");
-    price.classList.add("price");
-    price.textContent = `$${product.price.toFixed(2)}`;
-    pricesDiv.appendChild(listPrice);
-    pricesDiv.appendChild(price);
-    // Agregar el div de precio de descuento si el producto tiene descuento
-    if (product.discounted) {
-        productCard.classList.add("offer");
-        const priceTagDiv = document.createElement("div");
-        priceTagDiv.classList.add("price-tag");
-        const labelImg = document.createElement("img");
-        labelImg.src = "imgs/label.svg";
-        labelImg.alt = "";
-        const discountPercent = document.createElement("p");
-        discountPercent.classList.add("discount");
-        discountPercent.textContent = `10%`; // Mostrar el porcentaje de descuento real
-        priceTagDiv.appendChild(labelImg);
-        priceTagDiv.appendChild(discountPercent);
-        bottomSection.appendChild(priceTagDiv);
-        productCardDiscountContainer.appendChild(productCard);
-        console.log("Producto descuentado");
 
-    } else {
-        productCardContainer.appendChild(productCard);
-        console.log("Producto normal agregado");
+    const infoSection = createInfoSection(product);
+    const bottomSection = createBottomSection(product);
 
-    }
-    bottomSection.appendChild(addButton);
-    bottomSection.appendChild(pricesDiv);
     productCard.appendChild(infoSection);
     productCard.appendChild(bottomSection);
 
-    // Agregar la tarjeta de producto al contenedor
+    if (product.onSale) {
+        addDiscountedProduct(productCard);
+    } else {
+        addRegularProduct(productCard);
+    }
+}
+
+function createInfoSection(product) {
+    const infoSection = document.createElement("div");
+    infoSection.classList.add("info");
+
+    const imgElement = document.createElement("img");
+    imgElement.classList.add("imgs");
+    imgElement.src = product.image;
+    imgElement.alt = "";
+
+    const titleElement = document.createElement("h3");
+    titleElement.classList.add("title");
+    titleElement.textContent = product.title;
+
+    const idElement = document.createElement("p");
+    idElement.textContent = "id number";
+
+    infoSection.appendChild(imgElement);
+    infoSection.appendChild(titleElement);
+    infoSection.appendChild(idElement);
+
+    return infoSection;
+}
+
+function createBottomSection(product) {
+    const bottomSection = document.createElement("div");
+    bottomSection.classList.add("bottom");
+
+    const addButton = createAddButton();
+    const pricesDiv = createPricesDiv(product);
+
+    bottomSection.appendChild(addButton);
+    bottomSection.appendChild(pricesDiv);
+
+    return bottomSection;
+}
+
+function createAddButton() {
+    const addButton = document.createElement("button");
+    addButton.classList.add("addCart", "button-flex-container");
+
+    const shoppingBagIcon = createSVGIcon("shoppingBagIcon", 24, 26);
+    const plusIcon = createSVGIcon("plusIcon", 13, 12);
+
+    addButton.appendChild(shoppingBagIcon);
+    addButton.appendChild(plusIcon);
+
+    return addButton;
+}
+
+function createPricesDiv(product) {
+    const pricesDiv = document.createElement("div");
+    pricesDiv.classList.add("prices");
+
+    const listPrice = document.createElement("p");
+    listPrice.classList.add("listPrice");
+
+    const price = document.createElement("p");
+    price.classList.add("price");
+    price.textContent = `$${product.price.toFixed(2)}`;
+
+    pricesDiv.appendChild(listPrice);
+    pricesDiv.appendChild(price);
+
+    return pricesDiv;
+}
+
+function addDiscountedProduct(productCard) {
+    productCard.classList.add("offer");
+
+    const priceTagDiv = document.createElement("div");
+    priceTagDiv.classList.add("price-tag");
+
+    const labelImg = document.createElement("img");
+    labelImg.src = "imgs/label.svg";
+    labelImg.alt = "";
+
+    const discountPercent = document.createElement("p");
+    discountPercent.classList.add("discount");
+    discountPercent.textContent = `10%`;
+
+    priceTagDiv.appendChild(labelImg);
+    priceTagDiv.appendChild(discountPercent);
+
+    const bottomSection = productCard.querySelector(".bottom");
+    bottomSection.appendChild(priceTagDiv);
+
+    productCardDiscountContainer.appendChild(productCard);
+
+    console.log("Producto descuentado");
+}
+
+function addRegularProduct(productCard) {
+    productCardContainer.appendChild(productCard);
+
+    console.log("Producto normal agregado");
+}
+
+function createSVGIcon(id, width, height) {
+    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.id = id;
+    svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+    svg.setAttribute("width", width);
+    svg.setAttribute("height", height);
+
+    // Aquí puedes agregar el contenido SVG necesario para cada icono
+
+    return svg;
 }
 
 function updateProductCards() {
     products.forEach(addProductCard);
 }
 
-// Integrate the function to fetch data from the API and render products
 async function fetchDataAndRenderProducts() {
     try {
         const response = await fetch("https://64f659ae2b07270f705e6753.mockapi.io/api/products");
@@ -195,14 +231,14 @@ async function fetchDataAndRenderProducts() {
 
         if (Array.isArray(data)) {
             products = data.map(item => {
-                const price = parseFloat(item.price); // Convert the price property to a number
-                const isFeatured = item.featured === true; // Check if the product is featured
-                const isOnSale = item.onSale === true; // Check if the product is on sale
+                const price = parseFloat(item.price);
+                const isFeatured = item.featured === true;
+                const isOnSale = item.onSale === true;
 
                 return {
                     title: item.title || "",
                     description: item.description || "",
-                    price: isNaN(price) ? 0 : price, // Ensure price is a valid number
+                    price: isNaN(price) ? 0 : price,
                     image: item.image || "",
                     discounted: isOnSale,
                     featured: isFeatured,
